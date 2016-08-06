ff73cf583e61adedf3697e6b314ffca51c44a2f6
Rafael Jesus <rafaelljesus86@gmail.com>
2016-08-05 22:11:01 -0300

    Exposes a json

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
   "log"
   "net/http"
+  "io"
   "os"
 )
 
@@ -31,4 +32,7 @@ func (a *app) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 func (a *app) index(w http.ResponseWriter, r *http.Request) {
   a.log.Println("request to index")
+  w.WriteHeader(http.StatusOK)
+  w.Header().Set("Content-Type", "application/json")
+  io.WriteString(w, `{"alive": true}`)
 }
